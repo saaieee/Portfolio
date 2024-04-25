c9831dffefc5d437a2c454137c252d1d56f4decc
saai pranav <110209203+saaieee@users.noreply.github.com>
2024-04-26 01:20:01 +0530

    project template done copy over to rest of the projects

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const scrollLinks = document.querySelectorAll('.scrollcontainer a, .navbar a');
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollLinks = document.querySelectorAll(
+    ".scrollcontainer a, .navbar a"
+  );
 
-    scrollLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
+  scrollLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href");
+      const targetElement = document.querySelector(targetId);
 
-            if (targetElement) {
-                targetElement.scrollIntoView({
-                    behavior: 'smooth'
-                });
-            }
+      if (targetElement) {
+        targetElement.scrollIntoView({
+          behavior: "smooth",
         });
+      }
     });
+  });
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const readMoreBtn = document.querySelector('.read-more');
+  const fullText = document.querySelector('.full-text');
   
+  readMoreBtn.addEventListener('click', function() {
+      readMoreBtn.style.display = 'none'; 
+      fullText.style.display = 'block';   
+  });
+});
